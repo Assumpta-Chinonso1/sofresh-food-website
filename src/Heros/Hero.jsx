707f2f553953eedb90ad6fec810346c3deccce4d
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-06-19 13:42:33 +0100

    Responsiveness

--- src/Heros/Hero.jsx
+++ src/Heros/Hero.jsx
@@ -1,40 +1,35 @@
+
+
 import React, { useState } from 'react'
 import './Hero.css'
 import Order from '../Order/Order'
 
-
 const Hero = () => {
+  const [showPopUp, setShowPopUp] = useState(false)
 
-  const  [showPopUp, setShowPopUp] = useState(false)
-  
-    const handleOrderClick = () => {
-      setShowPopUp(true)
-    }
-  
-    const closePopUp = () => {
-      setShowPopUp(false)
-    }
-  
+  const handleOrderClick = () => {
+    setShowPopUp(true)
+  }
+
+  const closePopUp = () => {
+    setShowPopUp(false)
+  }
+
   return (
     <div className='Hero'>
-         <div className="hero">
-                  <h1>
-                    More Than Food. <br />   
-                    <span>It's A Lifestyle</span>
-                     </h1>
-              
-                     <br />
-                <button onClick={handleOrderClick} className='btn'>Order  Now</button> 
-                   {showPopUp && <Order onClose={closePopUp}/>}
-               </div>
-         
-              
-                
-               
+      <div className="hero">
+        <h1>
+          More Than Food. <br />
+          <span>It's A Lifestyle</span>
+        </h1>
+        <br />
+        <button onClick={handleOrderClick} className='btn'>Order Now</button>
+        {showPopUp && <Order onClose={closePopUp} />}
+      </div>
     </div>
-     
-
   )
 }
 
 export default Hero
+
+
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,38 +1,77 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../assets/assests'
-import { Link } from 'react-router-dom'
-import Menu from '../Menu/Menu'
-import Order from '../Order/Order'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../assets/assests';
+import { Link } from 'react-router-dom';
+import Order from '../Order/Order';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 const Navbar = () => {
-
-  const  [showPopUp, setShowPopUp] = useState(false)
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleOrderClick = () => {
-    setShowPopUp(true)
-  }
+    setShowPopUp(true);
+    setMenuOpen(false); 
+  };
 
   const closePopUp = () => {
-    setShowPopUp(false)
-  }
+    setShowPopUp(false);
+  };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <div className='navbar'>
-       <Link to='/'> <img src={assets.sofresh_logo} alt=" logo" className='logo' /> </Link>  
-            
-         <ul className="navbar-menu">
-             <li> <Link to='/menu' className='nav-link'> Our Menu  </Link> </li> 
-               <li> <Link to='/location' > Find Us  </Link></li>
-         </ul>
-         <div className="navbar-right">
-                  <img src={assets.search_icon} alt="" className='icon'/>
-                      </div>
-                      <button onClick={handleOrderClick} className='btn'>Order  Now</button>   
-                      {showPopUp && <Order onClose={closePopUp}/>}       
-    </div>
-  )
-}
+      <div className='navbar'>
+    <Link to='/'>
+      <img src={assets.sofresh_logo} alt="logo" className='logo' />
+    </Link>
 
-export default Navbar
+      {/* Desktop Nav Links */}
+      <ul className='navbar-menu desktop-only'>
+        <li><Link to='/menu'>Our Menu</Link></li>
+        <li><Link to='/location'>Find Us</Link></li>
+      </ul>
+
+       <div className={`hamburger mobile-only ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+      {menuOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+    </div>
+
+    {/* inside navbar */}
+    {menuOpen && <div className="mobile-top-line mobile-only"></div>}
+
+    
+      {/* Mobile Menu */}
+      {menuOpen && (
+        <ul className={`navbar-menu mobile-only open`}>
+           <div className="mobile-separator"></div>
+          <li><Link to='/menu'>Our Menu</Link></li>
+          <li><Link to='/location'>Find Us</Link></li>
+         
+          <li><img src={assets.search_icon} alt="search" className='icon' /></li>
+          <li><button onClick={handleOrderClick} className='btn'>Order Now</button></li>
+        </ul>
+      )}
+
+      {/* Desktop Right Side */}
+      <div className="navbar-right desktop-only">
+        <img src={assets.search_icon} alt="search" className='icon' />
+        <button onClick={handleOrderClick} className='btn'>Order Now</button>
+      </div>
+
+      {showPopUp && <Order onClose={closePopUp} />}
+    </div>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
