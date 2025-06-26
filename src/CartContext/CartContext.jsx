7665679166dbf46a18db66cec9ba511c2d2348db
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-06-26 05:41:24 +0100

    new commit

--- src/CartContext/CartContext.jsx
+++ src/CartContext/CartContext.jsx
@@ -1,51 +1,5 @@
 
 
-/*import React, { createContext, useState, useContext } from 'react';
-
-export const CartContext = createContext();
-
-export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (product) => {
-    const isAlreadyInCart = cartItems.find(item => item.id === product.id);
-    if (isAlreadyInCart) {
-      setCartItems(cartItems.map(item =>
-        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-      ));
-    } else {
-      setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    }
-  };
-
-  const removeFromCart = (id) => {
-    setCartItems(cartItems.filter(item => item.id !== id));
-  };
-
-  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
-
-  const getTotalCartAmount = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
-  return (
-    <CartContext.Provider
-      value={{
-        cartItems,
-        addToCart,
-        removeFromCart,
-        cartCount,
-        getCartItems: () => cartItems,
-        getTotalCartAmount // ✅ Make sure this is included
-      }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
-};
-
-export const useCart = () => useContext(CartContext);*/
-
 
 import React, { createContext, useState, useContext } from 'react';
 
